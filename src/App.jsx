db7058005557fbbcd78cc2e2f4e44d790fc3b8b8
Vince Chimento <vchimento@gmail.com>
2025-03-22 17:49:40 -0400

    implement Player Component logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Player from './components/Player.jsx';
 function App() {
-  return <p>Coming soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        GameBoard
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
